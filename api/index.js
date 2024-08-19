63246f55d2235435d51949972fc4761189dabcff
Saad Khaleeq <saadkhaleeq610@gmail.com>
2024-08-19 06:28:06 -0700

    auth controller & password bcrypt added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
@@ -10,7 +12,11 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 })
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000!');
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
